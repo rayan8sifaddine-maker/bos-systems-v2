c11542639c606823dc608228138c012b26a8e565
Rayan <rayan8sifaddine@gmail.com>
2026-06-11 13:53:16 +0100

    fix crm client

--- app/dashboard/crm/page.tsx
+++ app/dashboard/crm/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic'
 'use client'
+export const dynamic = 'force-dynamic'
 import { useState, useEffect } from 'react'
 import { initials, STATUS_COLORS, STATUS_LABELS } from '@/lib/utils'
 
